2e7668121836c32c7332d621377b3e0fe5cffaa0
Mark Chen <mchen@uts-canada.com>
2020-05-29 13:31:14 -0600

    Updates

---
+++ precache-manifest.b195cf05e0197cbac052ae25150ef9c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35bd5c830525bf4299a3dddfd02aa803",
+    "url": "/comedy-app/index.html"
+  },
+  {
+    "revision": "886b05addd305bf872ba",
+    "url": "/comedy-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "852f00ad2a5699b0ed16",
+    "url": "/comedy-app/static/js/2.66f8245d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/comedy-app/static/js/2.66f8245d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886b05addd305bf872ba",
+    "url": "/comedy-app/static/js/main.8e855c0c.chunk.js"
+  },
+  {
+    "revision": "4ab6bb74cee5071ddf48",
+    "url": "/comedy-app/static/js/runtime-main.885e550f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/comedy-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
